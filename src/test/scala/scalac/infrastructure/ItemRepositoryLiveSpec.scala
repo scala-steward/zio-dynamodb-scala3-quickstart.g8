9739572a09aa90486596da7191945ff7d97f7388
Franco Testagrossa <franco.testagrossa@scalac.io>
2024-01-21 22:10:17 +0400

    Move spec to use test executor

--- src/test/scala/scalac/infrastructure/ItemRepositoryLiveSpec.scala
+++ src/test/scala/scalac/infrastructure/ItemRepositoryLiveSpec.scala
@@ -7,35 +7,11 @@ import zio.test.Assertion._
 import zio.test.TestAspect._
 import java.util.UUID
 import scalac.infrastructure.dynamodb._
-import scalac.config.Configuration._
 import zio.aws.dynamodb.DynamoDb
 import zio.dynamodb.DynamoDBExecutor
-import zio.aws.core.config.AwsConfig
-import zio.aws.netty.NettyHttpClient
-import java.net.URI
-import software.amazon.awssdk.regions.Region
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
-
+import zio.dynamodb.TestDynamoDBExecutor
+import zio.dynamodb.{ Item => DynamoItem, PrimaryKey }
 object ItemRepositoryLiveSpec extends ZIOSpecDefault:
-
-  val containerLayer = ZLayer.scoped(DynamoContainer.make())
-
-  // FIXME: create table on init
-  val dynamoDbLayer: ZLayer[DynamoContainer, Throwable, DynamoDBExecutor] =
-    NettyHttpClient.default
-      >>> AwsConfig.default
-      >>> DynamoDb.customized { builder =>
-        builder
-          .endpointOverride(URI.create(s"http://0.0.0.0:4567"))
-          .region(Region.of("us-east-1"))
-          .credentialsProvider(
-            StaticCredentialsProvider
-              .create(AwsBasicCredentials.create("dummy", "dummy"))
-          )
-
-      }
-      >>> DynamoDBExecutor.live
 
   val repoLayer = ItemRepositoryLive.layer
 
@@ -45,44 +21,36 @@ object ItemRepositoryLiveSpec extends ZIOSpecDefault:
 
   override def spec =
     suite("item repository test with dynamodb test container")(
-      test("save items returns their ids") {
+      test("should support crud opperations") {
         for {
-          _ <- ItemRepository.add(Item(ItemId(id1), "first item", BigDecimal(1)))
-          _ <- ItemRepository.add(Item(ItemId(id2), "second item", BigDecimal(2)))
-          _ <- ItemRepository.add(Item(ItemId(id3), "third item", BigDecimal(3)))
-
-        } yield assert(ItemId(id1))(equalTo(ItemId(id1)))
-        && assert(ItemId(id2))(equalTo(ItemId(id2)))
-        && assert(ItemId(id3))(equalTo(ItemId(id3)))
-      },
-      test("get all returns 3 items") {
-        for {
-          items <- ItemRepository.getAll()
-        } yield assert(items)(hasSize(equalTo(3)))
-      },
-      test("delete first item") {
-        for {
-          _    <- ItemRepository.delete(ItemId(id1))
-          item <- ItemRepository.getById(ItemId(id1))
-        } yield assert(item)(isNone)
-      },
-      test("get item 2") {
-        for {
-          item <- ItemRepository.getById(ItemId(id2))
-        } yield assert(item)(isSome) &&
-        assert(item.get.name)(equalTo("second item")) &&
-        assert(item.get.price)(equalTo(BigDecimal("2")))
-      },
-      test("update item 3") {
-        for {
-          _    <- ItemRepository.update(ItemId(id3), ItemData("updated item", BigDecimal(3)))
-          item <- ItemRepository.getById(ItemId(id3))
-        } yield assert(item)(isSome) &&
-        assert(item.get.name)(equalTo("updated item")) &&
-        assert(item.get.price)(equalTo(BigDecimal(3)))
-      },
+          _            <- TestDynamoDBExecutor.addTable("Items", "id")
+          // save items returns their ids
+          _            <- ItemRepository.add(Item(ItemId(id1), "first item", BigDecimal(1)))
+          _            <- ItemRepository.add(Item(ItemId(id2), "second item", BigDecimal(2)))
+          _            <- ItemRepository.add(Item(ItemId(id3), "third item", BigDecimal(3)))
+          // get all returns 3 items
+          items        <- ItemRepository.getAll()
+          testResult1  <- assert(items)(hasSize(equalTo(3)))
+          // "delete first item"
+          _            <- ItemRepository.delete(ItemId(id1))
+          deleted      <- ItemRepository.getById(ItemId(id1))
+          testResult2  <- assert(deleted)(isNone)
+          // "get item 2"
+          fetchedItem2 <- ItemRepository.getById(ItemId(id2))
+          testResult3  <-
+            assert(fetchedItem2)(isSome) &&
+            assert(fetchedItem2.get.name)(equalTo("second item")) &&
+            assert(fetchedItem2.get.price)(equalTo(BigDecimal("2")))
+          // "update item 3"
+          _            <- ItemRepository.update(ItemId(id3), ItemData("updated item", BigDecimal(3)))
+          updated      <- ItemRepository.getById(ItemId(id3))
+          testResult4  <-
+            assert(updated)(isSome) &&
+            assert(updated.get.name)(equalTo("updated item")) &&
+            assert(updated.get.price)(equalTo(BigDecimal(3)))
+        } yield testResult1 && testResult2 && testResult3 && testResult4
+      }
     ).provideShared(
-      containerLayer,
-      dynamoDbLayer,
+      DynamoDBExecutor.test,
       repoLayer,
     ) @@ sequential
